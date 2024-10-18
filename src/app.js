90aba98d837f753879b83aea5d115ba49108c8e8
Naveen S Bandara <116500705+S-A-N-D-A-R-U@users.noreply.github.com>
2024-10-18 18:47:58 +0530

    install nodemon, update script, write request handler

--- src/app.js
+++ src/app.js
@@ -5,3 +5,12 @@ const app = express();
 app.listen(3000, () => {
     console.log("server is listening on port 3000..");
 });
+
+
+app.use("/", (req, res) => {
+    res.send("hello from sever!..");
+});
+
+app.use("/hello" , (req, res) => {
+    res.send("Hello from rout (/rout)");
+});
